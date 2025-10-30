111a34310c4e2cf1f24a738f20561c86fadfd732
marcosherna <marcosxdf6@gmail.com>
2025-10-30 15:32:06 -0600

    feat: add form login

--- frontend/mobile/src/screens/LoginScreen.tsx
+++ frontend/mobile/src/screens/LoginScreen.tsx
@@ -1,7 +1,7 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-import { Button } from "../components";
+import { Button, Input } from "../components";
 import { RootStackNavigation } from "../navigations/params";
 
 export default function LoginScreen() {
@@ -12,9 +12,40 @@ export default function LoginScreen() {
   };
 
   return (
-    <View>
-      <Text>Login</Text>
-      <Button title="Iniciar sesion" onPress={() => handleMainAppOnPress()} />
+    <View style={styles.container}>
+      <View style={styles.container_content}>
+        <Text style={styles.title}>Login</Text>
+        {/* TODO: place appropriate content, such as text or images */}
+      </View>
+
+      <View>
+        <Input label="Correo" placeholder="example.user@gmail.com" />
+        <Input label="Contrasenia" placeholder="********" />
+      </View>
+
+      <View style={{ marginTop: 10 }}>
+        <Button title="Iniciar sesion" onPress={() => handleMainAppOnPress()} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    padding: 16,
+  },
+  container_inputs: {
+    gap: 8,
+  },
+  container_content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+  },
+});
