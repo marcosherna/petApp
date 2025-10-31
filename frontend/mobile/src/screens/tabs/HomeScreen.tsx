f0bb0dcbea993f59bedf627ef2dbac4917a61083
marcosherna <marcosxdf6@gmail.com>
2025-10-31 10:56:00 -0600

    feat: example components

--- frontend/mobile/src/screens/tabs/HomeScreen.tsx
+++ frontend/mobile/src/screens/tabs/HomeScreen.tsx
@@ -1,19 +1,54 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
-import { Button } from "../../components";
+import { Button, IconButton, Label } from "../../components";
 import { useTheme } from "../../hooks/useTheme";
 
 export default function HomeScreen() {
-  const { theme, isDark, setMode } = useTheme();
+  const { isDark, setMode } = useTheme();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 8,
+      }}
+    >
+      <Label size="4xl">Home Screen</Label>
 
       <Button
         title={isDark ? "light" : "dark"}
         onPress={() => setMode(isDark ? "light" : "dark")}
       />
+
+      <Label size="lg">Icon button</Label>
+      <View style={{ flexDirection: "row", gap: 8 }}>
+        <IconButton
+          icon="Sun"
+          variant="contained"
+          onPress={() => console.log("Light")}
+        />
+        <IconButton
+          icon="Moon"
+          variant="outline"
+          onPress={() => console.log("Dark")}
+        />
+        <IconButton
+          icon="Heart"
+          variant="ghost"
+          color="#E4080A"
+          colorShape="#E4080A"
+          shape="circle"
+        />
+      </View>
+
+      <Label size="lg" align="center" paragraph color="gray">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, ex!
+        Explicabo nam, eligendi praesentium amet rem corrupti. Libero corrupti
+        aliquid eius laborum, voluptatem ducimus hic eligendi voluptatibus.
+        Ullam, sed unde.
+      </Label>
     </View>
   );
 }
